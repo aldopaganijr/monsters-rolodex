4c7617e1bfef5c8e1f5b09181541a88811f838a9
aldopaganijr <aldojpagani@gmail.com>
2022-10-20 16:40:12 -0400

    switched repo

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(){
+    super()
+      this.state = {
+        name:'Andrei'
+      }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Hi, {this.state.name}
+          </p>
+          <button onClick={() => {
+            this.setState({name:"bum"})
+          }}
+          >Change Name</button>
+        </header>
+      </div>
+    );
+  }
+
+  }
+
+export default App;
